80d44a4e0b5ca67dd7f31a3bbb8001e9b648225c
DevilVoice <14630905+devilvoice@user.noreply.gitee.com>
2025-05-11 18:59:27 +0800

    framework_init

--- WHUChat/WHUChat.Server/Program.cs
+++ WHUChat/WHUChat.Server/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using WHUChat.Server.Hubs;
+using WHUChat.Server.Data;
+using WHUChat.Server.Mappings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("Default"),
+        new MySqlServerVersion(new Version(8, 0, 29))));
+//builder.Services.AddScoped<IUserService, UserService>(); // 示例注入
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 var app = builder.Build();
 
@@ -19,10 +31,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
 
+app.MapHub<ChatHub>("/chatHub"); // SignalR 路由
 app.Run();
--- WHUChat/WHUChat.Server/obj/Debug/net8.0/WHUChat.Server.AssemblyInfo.cs
+++ WHUChat/WHUChat.Server/obj/Debug/net8.0/WHUChat.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WHUChat.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb78f4abcc5f2243763523b406bf520870fdf43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa845db5cd5f51c29aa47f63569234eedeb2933d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WHUChat.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WHUChat.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
